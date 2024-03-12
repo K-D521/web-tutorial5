f0b4e881b36dc81a9377316b512a3c6117f4d340
Gkuldeep <kl210309@dal.ca>
2024-03-12 01:05:08 -0300

    added testcases

--- Routes/route.js
+++ Routes/route.js
@@ -1,181 +1,117 @@
-// const express = require('express');
-// const router = express.Router();
-// const crypto = require("crypto");
-
-// users = [{
-//     email: "user1@email.com",
-//     firstName: "First1",
-//     id: "0001"
-// },
-
-// {
-//     email: "user2@email.com",
-//     firstName: "First2",
-//     id: "0002"
-// }]
-
-// router.get('/users', (req, res) => {
-//     res.status(200).json({
-//         message: "Getting users",
-//         success: true,
-//         users: users
-//     })
-// })
-
-// router.post('/add', (req, res) => {
-
-//     const input = req.body;
-
-//     const id = crypto.randomBytes(16).toString("hex");
-//     input["id"] = id
-
-//     if (input?.email && input?.firstName) {
-//         users.push(input)
-//         res.status(200).json({
-//             message: "User added",
-//             success: true
-//         })
-//     }
-//     else {
-//         res.status(501).json({
-//             message: "Error : User not added",
-//         })
-//     }
-// })
-
-// router.get('/user/:id', (req, res) => {
-
-//     const id = req.params.id;
-
-//     const fetchedUser = users.find(user => user.id === id)
-//     if (!fetchedUser) {
-//         return res.status(404).send("Error : User not found");
-//     }
-//     res.status(500).json({
-//         success: true,
-//         user: fetchedUser
-//     })
-// })
-
-// router.put('/update/:id', (req, res) => {
-//     console.log(req.body);
-//     const id = req.params.id;
-//     console.log(id);
-//     const { email, firstName } = req.body;
-//     const userIndex = users.findIndex(user => user.id === id)
-
-//     if (userIndex === -1) {
-//         return res.status(404).send('Error : User not found');
-//     }
-//     if (email) {
-//         users[userIndex].email = email;
-//     }
-//     if (firstName) {
-//         users[userIndex].firstName = firstName;
-//     }
-//     res.status(200).json({
-//         message: 'User updated',
-//         success: true
-//     });
-// });
-
-// module.exports = router
-
 const express = require("express");
 const router = express.Router();
 const crypto = require("crypto");
 
-const users = [
+const userData = [
   {
-    email: "user1@email.com",
-    firstName: "First1",
-    id: "0001",
+    email: "kuldeep@gmail.com",
+    firstName: "kuldeep",
+    userId: "1",
   },
   {
-    email: "user2@email.com",
-    firstName: "First2",
-    id: "0002",
+    email: "raj@gmail.com",
+    firstName: "raj",
+    userId: "2",
+  },
+  {
+    email: "kd@gmail.com",
+    firstName: "kd",
+    userId: "3",
+  },
+  {
+    email: "jeet@gmail.com",
+    firstName: "jeet",
+    userId: "4",
   },
 ];
 
 router.get("/users", (req, res) => {
   try {
     res.status(200).json({
-      message: "Getting users",
+      message: "Users retrieved",
       success: true,
-      users: users,
+      data: userData,
     });
-  } catch (error) {
+  } catch (err) {
     res.status(500).json({
-      message: `Error: ${error.message}`,
+      status: "error",
+      message: "Internal server error",
     });
   }
 });
 
 router.post("/add", (req, res) => {
   try {
-    const input = req.body;
-    const id = crypto.randomBytes(16).toString("hex");
-    input["id"] = id;
-
-    if (input?.email && input?.firstName) {
-      users.push(input);
-      res.status(200).json({
-        message: "User added",
-        success: true,
-      });
-    } else {
-      res.status(501).json({
-        message: "Error: User not added",
+    const newUser = req.body;
+    if (!newUser?.email || !newUser?.firstName) {
+      return res.status(400).json({
+        status: "error",
+        message: "Email and first name are required",
       });
     }
-  } catch (error) {
+    const userId = crypto.randomBytes(16).toString("hex");
+    newUser["userId"] = userId;
+    userData.push(newUser);
+    res.status(200).json({
+      message: "User added",
+      success: true,
+    });
+  } catch (err) {
     res.status(500).json({
-      message: `Error: ${error.message}`,
+      status: "error",
+      message: "Internal server error",
     });
   }
 });
 
-router.get("/user/:id", (req, res) => {
+router.get("/user/:userId", (req, res) => {
   try {
-    const id = req.params.id;
-    const fetchedUser = users.find((user) => user.id === id);
-    if (!fetchedUser) {
-      return res.status(404).send("Error: User not found");
+    const userId = req.params.userId;
+    const foundUser = userData.find((user) => user.userId === userId);
+    if (!foundUser) {
+      return res.status(404).json({
+        status: "error",
+        message: "User not found",
+      });
     }
     res.status(200).json({
       success: true,
-      user: fetchedUser,
+      user: foundUser,
     });
-  } catch (error) {
+  } catch (err) {
     res.status(500).json({
-      message: `Error: ${error.message}`,
+      status: "error",
+      message: "Internal server error",
     });
   }
 });
 
-router.put("/update/:id", (req, res) => {
+router.put("/update/:userId", (req, res) => {
   try {
-    const id = req.params.id;
+    const userId = req.params.userId;
     const { email, firstName } = req.body;
-    const userIndex = users.findIndex((user) => user.id === id);
+    const userIndex = userData.findIndex((user) => user.userId === userId);
 
     if (userIndex === -1) {
-      return res.status(404).send("Error: User not found");
+      return res.status(404).json({
+        status: "error",
+        message: "User not found",
+      });
     }
     if (email) {
-      users[userIndex].email = email;
+      userData[userIndex].email = email;
     }
     if (firstName) {
-      users[userIndex].firstName = firstName;
+      userData[userIndex].firstName = firstName;
     }
     res.status(200).json({
       message: "User updated",
       success: true,
     });
-  } catch (error) {
+  } catch (err) {
     res.status(500).json({
-      message: `Error: ${error.message}`,
+      status: "error",
+      message: "Internal server error",
     });
   }
 });
