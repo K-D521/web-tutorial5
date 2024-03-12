48c929da39d533662812003a5c76dec86d831d53
Gkuldeep <kl210309@dal.ca>
2024-03-12 00:34:23 -0300

    modified

--- Routes/route.js
+++ Routes/route.js
@@ -1,83 +1,183 @@
-const express = require('express');
+// const express = require('express');
+// const router = express.Router();
+// const crypto = require("crypto");
+
+// users = [{
+//     email: "user1@email.com",
+//     firstName: "First1",
+//     id: "0001"
+// },
+
+// {
+//     email: "user2@email.com",
+//     firstName: "First2",
+//     id: "0002"
+// }]
+
+// router.get('/users', (req, res) => {
+//     res.status(200).json({
+//         message: "Getting users",
+//         success: true,
+//         users: users
+//     })
+// })
+
+// router.post('/add', (req, res) => {
+
+//     const input = req.body;
+
+//     const id = crypto.randomBytes(16).toString("hex");
+//     input["id"] = id
+
+//     if (input?.email && input?.firstName) {
+//         users.push(input)
+//         res.status(200).json({
+//             message: "User added",
+//             success: true
+//         })
+//     }
+//     else {
+//         res.status(501).json({
+//             message: "Error : User not added",
+//         })
+//     }
+// })
+
+// router.get('/user/:id', (req, res) => {
+
+//     const id = req.params.id;
+
+//     const fetchedUser = users.find(user => user.id === id)
+//     if (!fetchedUser) {
+//         return res.status(404).send("Error : User not found");
+//     }
+//     res.status(500).json({
+//         success: true,
+//         user: fetchedUser
+//     })
+// })
+
+// router.put('/update/:id', (req, res) => {
+//     console.log(req.body);
+//     const id = req.params.id;
+//     console.log(id);
+//     const { email, firstName } = req.body;
+//     const userIndex = users.findIndex(user => user.id === id)
+
+//     if (userIndex === -1) {
+//         return res.status(404).send('Error : User not found');
+//     }
+//     if (email) {
+//         users[userIndex].email = email;
+//     }
+//     if (firstName) {
+//         users[userIndex].firstName = firstName;
+//     }
+//     res.status(200).json({
+//         message: 'User updated',
+//         success: true
+//     });
+// });
+
+// module.exports = router
+
+const express = require("express");
 const router = express.Router();
 const crypto = require("crypto");
 
-users = [{
+const users = [
+  {
     email: "user1@email.com",
     firstName: "First1",
-    id: "0001"
-},
-
-{
+    id: "0001",
+  },
+  {
     email: "user2@email.com",
     firstName: "First2",
-    id: "0002"
-}]
+    id: "0002",
+  },
+];
 
-router.get('/users', (req, res) => {
+router.get("/users", (req, res) => {
+  try {
     res.status(200).json({
-        message: "Getting users",
-        success: true,
-        users: users
-    })
-})
-
-router.post('/add', (req, res) => {
-
-    const input = req.body;
-
-    const id = crypto.randomBytes(16).toString("hex");
-    input["id"] = id
-
-    if (input?.email && input?.firstName) {
-        users.push(input)
-        res.status(200).json({
-            message: "User added",
-            success: true
-        })
-    }
-    else {
-        res.status(501).json({
-            message: "Error : User not added",
-        })
-    }
-})
-
-
-router.get('/user/:id', (req, res) => {
-
-    const id = req.params.id;
-
-    const fetchedUser = users.find(user => user.id === id)
-    if (!fetchedUser) {
-        return res.status(404).send("Error : User not found");
-    }
-    res.status(500).json({
-        success: true,
-        user: fetchedUser
-    })
-})
-
-router.put('/update/:id', (req, res) => {
-    console.log(req.body);
-    const id = req.params.id;
-    console.log(id);
-    const { email, firstName } = req.body;
-    const userIndex = users.findIndex(user => user.id === id)
-
-    if (userIndex === -1) {
-        return res.status(404).send('Error : User not found');
-    }
-    if (email) {
-        users[userIndex].email = email;
-    }
-    if (firstName) {
-        users[userIndex].firstName = firstName;
-    }
-    res.status(200).json({
-        message: 'User updated',
-        success: true
+      message: "Getting users",
+      success: true,
+      users: users,
     });
+  } catch (error) {
+    res.status(500).json({
+      message: `Error: ${error.message}`,
+    });
+  }
 });
 
-module.exports = router
+router.post("/add", (req, res) => {
+  try {
+    const input = req.body;
+    const id = crypto.randomBytes(16).toString("hex");
+    input["id"] = id;
+
+    if (input?.email && input?.firstName) {
+      users.push(input);
+      res.status(200).json({
+        message: "User added",
+        success: true,
+      });
+    } else {
+      res.status(501).json({
+        message: "Error: User not added",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: `Error: ${error.message}`,
+    });
+  }
+});
+
+router.get("/user/:id", (req, res) => {
+  try {
+    const id = req.params.id;
+    const fetchedUser = users.find((user) => user.id === id);
+    if (!fetchedUser) {
+      return res.status(404).send("Error: User not found");
+    }
+    res.status(200).json({
+      success: true,
+      user: fetchedUser,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: `Error: ${error.message}`,
+    });
+  }
+});
+
+router.put("/update/:id", (req, res) => {
+  try {
+    const id = req.params.id;
+    const { email, firstName } = req.body;
+    const userIndex = users.findIndex((user) => user.id === id);
+
+    if (userIndex === -1) {
+      return res.status(404).send("Error: User not found");
+    }
+    if (email) {
+      users[userIndex].email = email;
+    }
+    if (firstName) {
+      users[userIndex].firstName = firstName;
+    }
+    res.status(200).json({
+      message: "User updated",
+      success: true,
+    });
+  } catch (error) {
+    res.status(500).json({
+      message: `Error: ${error.message}`,
+    });
+  }
+});
+
+module.exports = router;
